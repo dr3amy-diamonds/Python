089671e45a2e5cea68868638f39cd7048ea193e3
dr3amy-diamonds <vale05ojeda@gmail.com>
2026-02-12 17:00:37 -0500

    ormato de nombres 01-09 con punto para orden ascendente

--- Proyectos/Proyectos/4.Contador de Caracteres en una cadena/4. Contador de Caracteres en una Cadena.py
+++
@@ -1,9 +0,0 @@
-#Crear funcion
-
-def cadena_texto():
-    txt = input("Introduce un texto, no importa el tamaño de este:\n ")
-    frase= input("Introduce la letra o palabra que quieras buscar:\n ")
-    conteo = txt.count(frase)
-    return(f"El número de veces que {frase} aparece en la cadena es:", conteo)
-
-print(cadena_texto())
